53d42f94ccc82184dd9c6c438ef8df9d19e94885
Marcell Varga <v.marcell4000@gmail.com>
2023-11-24 15:54:18 +0100

    better UX

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,17 @@
 use egui::{remap, vec2, Color32, RichText, Stroke};
 use egui_plot::{Line, LineStyle, Plot, PlotPoints, Points};
 
-use self::calculator2::{Calculator, Coordinates};
+use self::calculator::{Calculator, Coordinates};
 
 //use self::calculator::math_eng_init;
 
-mod calculator2;
+mod calculator;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct TemplateApp {
+    dot_color: Color32,
+    dot_size: f32,
     calc_cycles: f32,
     calc_step: f32,
     user_equation: String,
@@ -22,6 +24,8 @@ pub struct TemplateApp {
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
+            dot_color: Color32::WHITE,
+            dot_size: 1.,
             calc_cycles: 10.,
             calc_step: 1.,
             user_equation: String::new(),
@@ -57,20 +61,25 @@ impl eframe::App for TemplateApp {
 
                 let temp_string_size = self.user_equation.len();
 
+                let temp_cyc_size = self.calc_cycles;
+                let temp_step_size = self.calc_step;
+
                 ui.group(|ui| {
                     ui.label("Equation");
                     ui.text_edit_singleline(&mut self.user_equation);
                     ui.separator();
-                    ui.label(RichText::from("Output : ").strong().size(15.));
-                    ui.label(
-                        RichText::from(&self.math_output)
-                            .color(Color32::RED)
-                            .size(15.),
-                    );
+                    ui.menu_button("Help", |ui|{
+                        ui.label("Need help? Click on the template equations, to experiment with them.");
+                        let equations = ["1 / x", "sroot x", "croot x", "abs x", "x ^ 2"];
+                        for item in equations {
+                            if ui.button(item).clicked() {
+                                self.user_equation = item.to_string();
+                                self.calc();
+                            }
+                        }
+                        ui.label(RichText::from("Always make sure to separate the numbers and expressions with a space!").strong())
+                    });
                 });
-
-                let temp_cyc_size = self.calc_cycles;
-                let temp_step_size = self.calc_step;
 
                 ui.group(|ui| {
                     ui.label("Limit");
@@ -89,18 +98,19 @@ impl eframe::App for TemplateApp {
                 .response
                 .on_hover_text("Settings for the calculation");
 
+                ui.group(|ui|{
+                    ui.label("Dot size");
+                    ui.add(egui::DragValue::new(&mut self.dot_size).clamp_range(0.1..=f64::MAX));
+                    ui.label("Dot color");
+                    ui.color_edit_button_srgba(&mut self.dot_color);
+                });
+
                 //call calculator on every change
                 if temp_string_size != self.user_equation.len()
                     || temp_cyc_size != self.calc_cycles
                     || temp_step_size != self.calc_step
                 {
-                    //self.equation_vec = math_eng_init(self.user_equation.clone(), self.calc_step.clone(), self.calc_cycles.clone());
-                    self.equation_vec = Calculator::init(&mut Calculator {
-                        buf: self.user_equation.clone(),
-                        bounds: self.calc_cycles,
-                        step: self.calc_step,
-                        ..Default::default()
-                    } );
+                    self.calc();
                 }
             });
         //dbg!(self.equation_vec.clone());
@@ -118,12 +128,21 @@ impl eframe::App for TemplateApp {
                                 [vector[item][0], vector[item][1]].into();
                             plot_ui.line(
                                 Line::new(line_points)
-                                    .color(Color32::WHITE)
-                                    .stroke(Stroke::new(10., Color32::RED)),
+                                    .stroke(Stroke::new(self.dot_size, self.dot_color)),
                             );
                         }
                     }
                 });
         });
+    }
+}
+impl TemplateApp {
+    fn calc(&mut self) {
+        self.equation_vec = Calculator::init(&mut Calculator {
+            buf: self.user_equation.clone(),
+            bounds: self.calc_cycles,
+            step: self.calc_step,
+            ..Default::default()
+        } );
     }
 }
